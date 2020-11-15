9b0a8f5cbd2a92b5b78ec75bbc8e1027377146e2
Marie <Vous@exemple.com>
2020-11-15 21:07:10 +0100

    home

--- front-dizify-music/src/app/Home/index.tsx
+++ front-dizify-music/src/app/Home/index.tsx
@@ -1,30 +1,154 @@
 import { RouteComponentProps } from "@reach/router";
-import { Tabs } from "antd/lib";
-import React, { FC } from "react";
-import AlbumsList from "../../albums/albums";
-import ArtistsList from "../../artists/artists";
+import { Card, Image } from "antd";
+import React, { FC, useEffect, useState } from "react";
+import albumsService from "../../services/albumsService";
+import artistService from "../../services/artistService";
 
-const { TabPane } = Tabs;
+type Artist = {
+  id: number;
+  name?: string;
+  imageUri?: string;
+};
+
+type Song = {
+  id?: number;
+  duration?: string;
+  name?: string;
+};
+
+type Album = {
+  id?: number;
+  name?: string;
+  pictureUri?: string;
+  artist?: Artist;
+  songs: Array<Song>;
+};
 
 const Home: FC<RouteComponentProps> = () => {
+  const [Albums, setAlbums] = useState<Album[]>([]);
+  const [Artists, setArtists] = useState<Artist[]>([]);
+
+  useEffect(() => {
+    albumsService.getAlbums().then((res) => {
+      const Albums = res.data;
+      setAlbums(Albums);
+    });
+    artistService.getArtists().then((res) => {
+      const Artists = res.data;
+      setArtists(Artists);
+    });
+  }, [Albums, Artists]);
+
+  function getRandomInt(max: any) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   return (
-    <div
-      style={{
-        width: "90%",
-        marginRight: "auto",
-        marginLeft: "auto",
-        marginTop: "2%",
-      }}
-    >
-      <Tabs defaultActiveKey="1" centered>
-        <TabPane tab="Artists" key="1">
-          <ArtistsList />
-        </TabPane>
-        <TabPane tab="Albums" key="2">
-          <AlbumsList />
-        </TabPane>
-      </Tabs>
-    </div>
+    <>
+      {" "}
+      <h1
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: 55,
+          marginTop: 50,
+          color: "var(--pink)",
+        }}
+      >
+        Bienvenue sur DizifyMusic
+      </h1>
+      <h2
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: 40,
+          marginTop: 50,
+          marginBottom: 50,
+        }}
+      >
+        {" "}
+        Artistes qui peuvent vous plaire{" "}
+      </h2>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          marginTop: "1%",
+          justifyContent: "center",
+        }}
+      >
+        {Artists.slice(0, 3).map((artist, i) => (
+          <>
+            <Card
+              style={{
+                width: 300,
+                marginRight: "1%",
+                marginBottom: "1%",
+                boxShadow: "0px 4px 100px -64px rgba(0,0,0,0.35)",
+              }}
+            >
+              <h1 style={{ fontWeight: "bold", fontSize: 30 }}>
+                {artist.name}
+              </h1>
+              <Image
+                width={200}
+                src={`${artist.imageUri}`}
+                style={{ marginBottom: "2%" }}
+              />
+            </Card>
+          </>
+        ))}
+      </div>
+      <h2
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: 40,
+          marginTop: 50,
+          marginBottom: 50,
+        }}
+      >
+        {" "}
+        Albums qui peuvent vous plaire{" "}
+      </h2>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          marginTop: "1%",
+          justifyContent: "center",
+        }}
+      >
+        {Albums.slice(0, 3).map((album, i) => (
+          <>
+            <Card
+              style={{
+                width: 300,
+                marginRight: "1%",
+                marginBottom: "1%",
+                boxShadow: "0px 4px 100px -64px rgba(0,0,0,0.35)",
+              }}
+            >
+              <h1 style={{ fontWeight: "bold", fontSize: 30 }}>{album.name}</h1>
+              <h2
+                style={{
+                  color: "var(--blue)",
+                  fontSize: 16,
+                  marginBottom: "5%",
+                }}
+              >
+                {album.artist?.name}
+              </h2>
+              <Image
+                width={200}
+                src={`${album.pictureUri}`}
+                style={{ marginBottom: "2%" }}
+              />
+            </Card>
+          </>
+        ))}
+      </div>
+    </>
   );
 };
 
--- front-dizify-music/src/index.tsx
+++ front-dizify-music/src/index.tsx
@@ -1,13 +1,46 @@
+import { Tabs } from "antd";
 import React from "react";
 import ReactDOM from "react-dom";
+import AlbumsList from "./albums/albums";
 import Home from "./app/Home";
+import ArtistsList from "./artists/artists";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/index.less";
 import "./styles/index.scss";
 
+const { TabPane } = Tabs;
+
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <div
+      style={{
+        width: "90%",
+        marginRight: "auto",
+        marginLeft: "auto",
+        marginTop: "2%",
+      }}
+    >
+      <Tabs defaultActiveKey="1" centered>
+        <TabPane tab="Accueil" key="1">
+          <Home />
+        </TabPane>
+        <TabPane tab="Compte utilisateur" key="2">
+          Se connecter
+        </TabPane>
+        <TabPane tab="Artists" key="3">
+          <ArtistsList />
+        </TabPane>
+        <TabPane tab="Albums" key="4">
+          <AlbumsList />
+        </TabPane>
+        <TabPane tab="Favoris" key="5">
+          Favoris
+        </TabPane>
+        <TabPane tab="Playlists" key="6">
+          Playlists
+        </TabPane>
+      </Tabs>
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
