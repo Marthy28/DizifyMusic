71e7566ad31b54a475503c965f17ff83383a5dfd
thomas_mary <thomas_mary@groupevsc.com>
2020-11-19 09:23:25 +0100

    clever cloud

--- WebService/src/main/java/com/ynov/dizifymusic/repository/UserRepository.java
+++ WebService/src/main/java/com/ynov/dizifymusic/repository/UserRepository.java
@@ -12,4 +12,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	@Query("SELECT a FROM User a WHERE a.eMail LIKE %:email%")
     public User findByEMail(@Param("email") String emailUser);
 	
+	
 }
